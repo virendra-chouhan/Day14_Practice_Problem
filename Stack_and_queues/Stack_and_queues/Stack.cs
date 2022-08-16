ac487f04dcda6a055fd545557ba17f6dd18e86fc
virendra <virendrachouhan1494@gmail.com>
2022-08-16 11:47:11 +0530

    UC1

---
+++ Stack_and_queues/Stack_and_queues/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_and_queues
+{
+    class Stack
+    {
+        public Node top;
+
+        public Stack()
+        {
+            top = null;
+        }
+        public void push(int value)
+        {
+            Node node = new Node(value);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+
+        }
+
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
